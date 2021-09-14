46327b3b0e667cef444511e211e599a4e972ab97
C W <claudiaywong@msn.com>
2021-09-14 01:17:34 +0000

    new changes to assertEqual.js - commented out test code

--- assertEqual.js
+++ assertEqual.js
@@ -10,8 +10,9 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
+/*
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 18);
-
+*/
