8e24fb1dccd2ac995f49170dde9dc06c0247ee8f
C W <claudiaywong@msn.com>
2021-09-14 22:47:54 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays = function(array1, array2) {
+  //first, check that the size of the array is the same
+  if (array1.length !== array2.length) {
+    console.log("Both arrays are different lengths. Please input two arrays of the same lenth.");
+    return false;
+  }
+  //Loop through the arrays to check element by element
+  for (let i = 0; i < array1.length; i++) {
+    //console.log("comparing elements:", array1[i], array2[i]);
+    if (array1[i] !== array2[i]) {
+      //if the two elements at the same index do not match, return false
+      return false;
+    }
+    return true;
+  }
+};
+//TEST Code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 42]), false); // => should PASS
