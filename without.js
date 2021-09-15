64530eab27799d5f7c809a5f4c0525464711c4b9
C W <claudiaywong@msn.com>
2021-09-15 00:14:48 +0000

    added without.js

---
+++ without.js
@@ -0,0 +1,51 @@
+let eqArrays = function(array1, array2) {
+  //first, check that the size of the array is the same
+  if (array1.length !== array2.length) {
+    console.log("Both arrays are different lengths. Please input two arrays of the same lenth.");
+    return false;
+  }
+  //Loop through the arrays to check element by element
+  for (let i = 0; i < array1.length; i++) {
+    //console.log("comparing elements:", array1[i], array2[i]);
+    if (array1[i] !== array2[i]) {
+      //if the two elements at the same index do not match, return false
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
+    //console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    //console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+let without = function(source, itemsToRemove) {
+  let parsedArray = source;
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (itemsToRemove[j] === source[j]) {
+        parsedArray.splice(j,1);
+      }
+    }
+  }
+  return parsedArray;
+};
+
+// //Test code
+// console.log(without([1, 2, 3], [1])); // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+// console.log(without([1, 2, 3], [])); // => [1, 2, 3]
+// console.log(without([1, 2, 3], ['hello'])); // => [1, 2, 3]
+// console.log(without([], [1,2,3])); // => []
+
