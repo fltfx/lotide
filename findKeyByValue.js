6d090d7c350c30eafa74717cbdb31c6beb37132d
C W <claudiaywong@msn.com>
2021-09-16 22:19:30 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let findKeyByValue = function(obj, val) {
+  //use For-In loop to loop through the object
+  for (let key in obj) {
+    if (val === obj[key]) {
+      return key;
+    }
+  }
+  //else, if cannot find a key for that value:
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  romance: "Portlandia",
+  historical: "Portlandia"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Portlandia"), "romance");
