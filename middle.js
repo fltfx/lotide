601882e1c85cc936032850e954d202e3a416c743
C W <claudiaywong@msn.com>
2021-09-21 15:56:17 +0000

    refactored middle

--- middle.js
+++ middle.js
@@ -1,30 +1,5 @@
-let eqArrays = function(array1, array2) {
-  //first, check that the size of the array is the same
-  if (array1.length !== array2.length) {
-    console.log("Both arrays are different lengths. Please input two arrays of the same lenth.");
-    return false;
-  }
-  //Loop through the arrays to check element by element
-  for (let i = 0; i < array1.length; i++) {
-    //console.log("comparing elements:", array1[i], array2[i]);
-    if (array1[i] !== array2[i]) {
-      //if the two elements at the same index do not match, return false
-      return false;
-    }
-  }
-  return true;
-};
-
-let assertArraysEqual = function(array1, array2) {   //OLD CODE: const assertEqual = function(actual, expected) {
-
-  if (eqArrays(array1,array2)) {    // OLD CODE: if (actual === expected) {
-    //console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    //console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 let middle = function(array) {
   let middleArray = [];
@@ -41,9 +16,6 @@ let middle = function(array) {
   return middleArray;
 };
 
-// //Test code
-// assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-// assertArraysEqual(middle(["1", "2", "3"]),["2"]); // => ["2"]
-// assertArraysEqual(middle([1, 2, 3,4]), [2,3]); // => [2, 3]
-// assertArraysEqual(middle([]),[]); // => []
-// assertArraysEqual(middle([1,2]),[]); // => []
+module.exports = middle;
+
+
---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual')
+
+//Test code
+assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
+assertArraysEqual(middle(["1", "2", "3"]),["2"]); // => ["2"]
+assertArraysEqual(middle([1, 2, 3,4]), [2,3]); // => [2, 3]
+assertArraysEqual(middle([]),[]); // => []
+assertArraysEqual(middle([1,2]),[]); // => []
